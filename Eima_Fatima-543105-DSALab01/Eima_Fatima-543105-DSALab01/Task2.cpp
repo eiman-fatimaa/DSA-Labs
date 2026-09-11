263fe7f78fceb009bd92a3fa3a7a091908d0a060
eiman-fatimaa <zainabmaha180@gmail.com>
2026-09-11 15:58:12 +0500

    all tasks

---
+++ Eima_Fatima-543105-DSALab01/Eima_Fatima-543105-DSALab01/Task2.cpp
@@ -0,0 +1,16 @@
+// Eiman Fatima
+#include <iostream>
+using namespace std;
+int main() {
+
+	int numbers[5];
+	int total = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> numbers[i];
+	}
+	for (int i = 0; i < 5; i++) {
+		total += numbers[i];
+	}
+	cout << total;
+	return 0;
+}
