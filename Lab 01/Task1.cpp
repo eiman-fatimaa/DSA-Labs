2ad522004f38ba0104de87b55e26b76a8b5c4910
Eiman Fatima <zainabmaha180@gmail.com>
2026-09-11 16:42:31 +0500

    Task 1 complete

---
+++ Lab 01/Task1.cpp
@@ -0,0 +1,14 @@
+// Eiman Fatima
+#include <iostream>
+using namespace std;
+int main() {
+
+	int numbers[5] = { 2,4,6,8,10 };
+	numbers[2] = 7;
+
+	for (int i = 0; i < 5; i++) {
+		cout << numbers[i] << " ";
+	}
+	
+	return 0;
+}
